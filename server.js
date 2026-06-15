fe13e982bf08a1f9b243c5b9f0dba60c4c9b2ec6
Nick Meinhold <nick@enspyr.co>
2026-06-16 09:51:46 +1000

    refactor: extract ytSearch (ytSearch.js) from server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const net = require('net');
-const { spawn, execFile } = require('child_process');
+const { spawn } = require('child_process');
 const crypto = require('crypto');
 
 const PORT = Number(process.env.PORT || 3000);
@@ -564,32 +564,7 @@ async function playNext() {
 }
 
 // --- yt-dlp search ---
-function ytSearch(query) {
-  return new Promise((resolve) => {
-    execFile('yt-dlp', [
-      '--flat-playlist', '--dump-json', '--no-warnings',
-      '--default-search', 'ytsearch5',
-      `ytsearch5:${query}`
-    ], { maxBuffer: 1024*1024*8, timeout: 15000 }, (err, stdout) => {
-      if (err) { console.error('yt-dlp search err', err.message); return resolve([]); }
-      const lines = stdout.split('\n').filter(l => l.trim());
-      const out = [];
-      for (const l of lines) {
-        try {
-          const j = JSON.parse(l);
-          out.push({
-            videoId: j.id,
-            title: j.title,
-            thumbnail: j.thumbnails?.[0]?.url || `https://i.ytimg.com/vi/${j.id}/mqdefault.jpg`,
-            duration: j.duration,
-            channel: j.channel || j.uploader,
-          });
-        } catch(e) {}
-      }
-      resolve(out);
-    });
-  });
-}
+const { ytSearch } = require('./ytSearch');
 
 // --- consented spotlight research and facilitation ---
 async function fetchRemote(url, options = {}, timeoutMs = 9000) {
